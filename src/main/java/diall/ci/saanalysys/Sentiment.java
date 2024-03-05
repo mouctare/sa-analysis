aac9af72becbfc9ca48cfa079f509fc276fcb7e0
mouctare <mouctard78@gmail.com>
2024-03-05 10:45:24 +0100

    Github actions

--- src/main/java/diall/ci/saanalysys/Sentiment.java
+++ src/main/java/diall/ci/saanalysys/Sentiment.java
@@ -1,8 +1,13 @@
 package diall.ci.saanalysys;
 
-public record Sentiment(
-        int id,
-        String message,
-        int status
-) {
+public class Sentiment {
+    int id;
+    String message;
+    int status;
+
+    public Sentiment(int id, String message, int status) {
+        this.id = id;
+        this.message = message;
+        this.status = status;
+    }
 }
